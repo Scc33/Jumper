20e90105045559fc1d8eaf2ae2a44a5a37696032
Sean Coughlin <seanmc4@illinois.edu>
2019-04-26 09:10:19 -0500

    Test reading and writing scores

--- finalProject/test/test.cpp
+++ finalProject/test/test.cpp
@@ -12,3 +12,42 @@ int main(int argc, char* argv[]) {
 TEST_CASE("Test") {
     REQUIRE(1 == 1);
 }
+
+TEST_CASE("Read and writing scores") {
+    std::vector<int> highScores;
+    std::vector<std::string> highScoreNames;
+    
+    SECTION("Read score") {
+        REQUIRE(loader::ReadScores("/Users/coughlin/Documents/School/CS 126 C++/of_v0.10.1_osx_release/apps/myApps/final-project-Scc33/finalProject/bin/data/highScores.txt", highScores, highScoreNames) == true);
+    }
+    
+    SECTION("Write scores") {
+        REQUIRE(loader::WriteScores("/Users/coughlin/Documents/School/CS 126 C++/of_v0.10.1_osx_release/apps/myApps/final-project-Scc33/finalProject/bin/data/highScores.txt", highScores, highScoreNames) == true);
+    }
+}
+
+TEST_CASE("Other reading and writing") {
+    SECTION("Write settings") {
+        
+    }
+    
+    SECTION("Read settings") {
+        
+    }
+    
+    SECTION("Write market") {
+        
+    }
+    
+    SECTION("Read market") {
+        
+    }
+    
+    SECTION("Write money") {
+        
+    }
+    
+    SECTION("Read money") {
+        
+    }
+}
